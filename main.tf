9f0c888f3ac6335c8de7c037b4c6ab7d45b2b15d
scarface7rl <marco.goncalves13375660@gmail.com>
2022-07-26 17:30:22 -0300

    agora vai

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     minio = {
       # ATTENTION: use the current version here!
-      version = "0.1.0-alpha5"
       source  = "refaktory/minio"
     }
   }
@@ -13,54 +12,13 @@ resource "minio_bucket" "bucket" {
   name = "bucket"
 }
 
-# Create a policy.
-resource "minio_canned_policy" "policy1" {
-  name = "policy1"
-  policy = <<EOT
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "arn:aws:s3:::my-bucketname/*"
-      ]
-    }
-  ]
-}
-EOT
-}
-
-# Create a user group and assign the specified policies.
-resource "minio_group" "group1" {
-  name = "group1"
-  policies = [minio_canned_policy.policy1.name]
-}
-
 resource "minio_group" "group2" {
   name = "group2"
-}
-
-
-# Import an existing policy.
-# (the consoleAdmin policy is created by Minio automatically)
-data "minio_canned_policy" "console_admin" {
-  name = "consoleAdmin"
 }
 
 # Create a user with specified access credentials, policies and group membership.
 resource "minio_user" "user1" {
   access_key = "00000001"
   secret_key = "00000001"
-  policies = [
-    minio_canned_policy.policy1.name,
-    # Note: using a data source here!
-    data.minio_canned_policy.console_admin.name,
-  ]
-  groups = [
-    minio_group.group2.name,
-  ]
+
 }
