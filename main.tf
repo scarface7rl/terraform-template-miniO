7c93d1c0d7f85636474109b04bb3364514885bc9
scarface7rl <marco.goncalves13375660@gmail.com>
2022-07-26 13:15:44 -0300

    tomara q de

--- main.tf
+++ main.tf
@@ -7,17 +7,24 @@ terraform {
   }
 }
 
-provider "minio" {
-  access_key = var.username
-  # Specify your minio user secret key here.
-  secret_key = var.password
-  # If true, the server will be contacted via https://
-  ssl = false
-}
-
-# Create a bucket.
-
 resource "minio_bucket" "bucket" {
   # count = var.enableMinio ? 1 : 0
   name = var.name
 }
+
+resource "minio_user" "user" {
+  access_key = var.username
+  secret_key = var.password
+  policies = [
+    minio_canned_policy.policy.name
+    # Note: using a data source here!
+    #data.minio_canned_policy.console_admin.name,
+  ]
+  /*groups = [
+    minio_group.group2.name,
+  ]*/
+  depends_on = [
+    minio_canned_policy.policy,
+  ]
+}
+
